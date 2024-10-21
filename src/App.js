ebeb0c4228b4968de3c1b2fcfed31edb55f80a90
vietha1710 <87017313+vietha1710@users.noreply.github.com>
2024-10-21 22:01:15 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import * as React from 'react';
 import ButtonAppBar from './components/navbar/navbar';
 import NewsCard from './components/card/card'
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 function App() {
   return (
-    <Container className="App">
+    <React.Fragment>
       <ButtonAppBar/>
-      <NewsCard/>
-      <NewsCard/>
-      <NewsCard/>
-      <NewsCard/>
-    </Container>
+      <Container maxWidth="lg">
+      </Container>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ justifyContent: 'center', alignItems: "center" }}>
+        <Grid item xs={3}>
+          <NewsCard imageLink='https://cdn.loveandlemons.com/wp-content/uploads/2021/06/summer-desserts.jpg' />
+        </Grid>
+        <Grid item xs={3}>
+          <NewsCard />
+        </Grid>
+        <Grid item xs={3}>
+          <NewsCard />
+        </Grid>
+      </Grid>
+    </React.Fragment>
   );
 }
 
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -5,14 +5,14 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
-export default function NewsCard() {
+export default function NewsCard({imageLink}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="https://cdn.loveandlemons.com/wp-content/uploads/2021/06/summer-desserts.jpg"
+          image={imageLink}
           alt="green iguana"
         />
         <CardContent>
