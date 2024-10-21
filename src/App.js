86aa2709c0436bfca5697d16dcc291b30ad9b677
vietha1710 <87017313+vietha1710@users.noreply.github.com>
2024-10-21 20:50:11 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+import ButtonAppBar from './component/navbar/navbar';
+import NewsCard from './component/card/card'
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar/>
+      <NewsCard/>
+      <NewsCard/>
+      <NewsCard/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/component/card/card.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+export default function NewsCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://cdn.loveandlemons.com/wp-content/uploads/2021/06/summer-desserts.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
